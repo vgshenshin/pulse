7f2c5712697947f27df653784cf756f6589bf53a
vgshenshin <vg.shenshin@gmail.com>
2022-05-16 13:52:27 +0300

    add smtp, favicon

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//       speed: 1200,
-//       adaptiveHeight: true,
-//       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-//       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-//       responsive: [
-//         {
-//           breakpoint: 992,
-//           settings: {
-//             dots: true,
-//             arrows: false
-//           }
-//         }
-//       ]
-//     });
-//   });
 const slider = tns({
 	container: '.carousel__inner',
 	items: 1,
@@ -97,5 +80,59 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn('slow');
 		});
 	});
+
+	function valideForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите более {0}-х символов!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+					required: "Пожалуйста, введите свою почту",
+					email: "Неправильно введен адрес почты"
+				}
+			  }
+		});
+	};
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+
+	$("input[name=phone]").mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+	
 });
 
