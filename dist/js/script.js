262e51f41cde59c403fa6b8f6551b310def012af
vgshenshin <vg.shenshin@gmail.com>
2023-01-24 17:58:14 +0300

    add tiny-slider, photos in catalog

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,28 @@
+const slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: true,
+	autoplayTimeout: 2500,
+	controls: false,
+	navPosition: "bottom"
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
+
+document.querySelector('[data-action="stop"]').style.display = 'none';
+
+
 $(document).ready(function(){
 
-	$('.carousel__inner').slick({
+// slick slider
+/* 	$('.carousel__inner').slick({
  		autoplay: true,
 		autoplaySpeed: 2200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
@@ -21,13 +43,32 @@ $(document).ready(function(){
 				}
 			}
 		]
-	});
+	}); */
 
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 		$(this)
 			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
+
+//Первый упрощенный вариант исполнения переключения контента карточки товара без функции
+/* 	$('.catalog-item__link').each(function(i) {
+		$(this).on('click', function(e){
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+		})
+	});
+
+	$('.catalog-item__back').each(function(i) {
+		$(this).on('click', function(e){
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+		})
+	}); */
+
+	//Оптимизировали код с помощью функции
 
 	function toggleSlide(item) {
 		$(item).each(function(i) {
@@ -50,6 +91,10 @@ $(document).ready(function(){
 	$('.modal__close').on('click', function() {
 		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
 	});
+
+	/* 	$('.button_mini').on('click', function() {
+		$('.overlay, #order').fadeIn('slow');
+	}); */
 
 	$('.button_mini').each(function(i) {
 		$(this).on('click', function() {
